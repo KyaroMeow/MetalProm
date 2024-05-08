ef5d0b698e875f7758ae367a3183aff9188e2a2a
KyaroMeow <kirillid2006@gmail.com>
2024-05-08 10:38:24 +0300

    initial commit

---
+++ MetalProm/MetalProm/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetalProm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Craft_click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void AddMaterial_click(object sender, EventArgs e)
+        {
+            Form form2 = new Form2();
+            form2.Show();
+        }
+    }
+}
